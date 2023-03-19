7d39b289b92fc8a2e6f9937e7884390ed4d06a51
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-03-19 19:19:39 +0530

    Implemented row in homepage

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner';
 import Navbar from '../../components/Navbar/Navbar';
+import Row from '../../components/Row/Row';
+import request from '../../request';
 
 import './HomeScreen.css';
 
@@ -9,6 +11,12 @@ const HomeScreen = () => {
     <div className='homeScreen'>
         <Navbar />
         <Banner />
+        <Row endpoint={request.fetchNetflixOrignals} isLargeRow title='Netflix Orignals' />
+        <Row endpoint={request.fetchTrending} title='Now Trending' />
+        <Row endpoint={request.fetchTopRated} title='Top Rated' />
+        <Row endpoint={request.fetchActionMovies} title='Action Movies' />
+        <Row endpoint={request.fetchComedyMovies} title='Comedy Movies' />
+        <Row endpoint={request.fetchHorrorMovies} title='Horror Movies' />
     </div>
   )
 }
